3824e51de28eb5bd164efc7dc3e6ed89edfb5372
Josh Fraser <josh_fraser@ionburst.io>
2023-02-20 16:26:56 +0000

    update ionfs-develop to 0.3.1-develop.1

--- ionfs-develop.rb
+++ ionfs-develop.rb
@@ -1,5 +1,5 @@
 class IonfsDevelop < Formula
-    version = "0.3.0-develop.5"
+    version = "0.3.1-develop.1"
   
     desc "IonFS CLI (develop branch)"
     homepage "https://github.com/ionburstcloud/ionfs"
@@ -10,18 +10,18 @@ class IonfsDevelop < Formula
     if OS.mac?
       if Hardware::CPU.arm?
         url "https://ionfs.s3.fr-par.scw.cloud/develop/ionfs-#{version}-mac-arm64.tar.bz2"
-        sha256 "c63d725f7ee804b3d1fc6b6c9316ca752319873cd75fb5f95c5207514edfc639"
+        sha256 "aee85d4c287681de010d1685e527a230ba901b9c00bf549206ea629645123e08"
       else
         url "https://ionfs.s3.fr-par.scw.cloud/develop/ionfs-#{version}-mac.tar.bz2"
-        sha256 "f6ac8494bd06c585abe1bc1743cc701eb70212d9430ed7f1328f2dba8a8f194a"
+        sha256 "fca2f5657ee775cfddf08829e0df2954600393bda50cee51c1929fb3b94cc2e9"
       end
     elsif OS.linux?
       if Hardware::CPU.arm?
         url "https://ionfs.s3.fr-par.scw.cloud/develop/ionfs-#{version}-linux-arm64.tar.bz2"
-        sha256 "8aa9a022b8be4640ab42b69520d18f1df763275b65a944ba15c615e4549e83d5"
+        sha256 "cafa5f252969fd0353f007a63260ed491d2070a58fc50aff29998ce1334e7b3d"
       else
         url "https://ionfs.s3.fr-par.scw.cloud/develop/ionfs-#{version}-linux.tar.bz2"
-        sha256 "b89f56e9f05626da4465146812d52787a8880a77ee72760dfb089fd96e0cd25a"
+        sha256 "501ca17d7874fd630b7970dbe077871557d109f978c30d81b70634f20431f5c5"
       end
     end
   
